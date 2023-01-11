92fb8bfa797de974eb0694866fd72c362a8fcbff
harii84 <99866727+harii84@users.noreply.github.com>
2023-01-11 22:09:17 +0530

    add product card style in home page

--- src/firebase.js
+++ src/firebase.js
@@ -23,5 +23,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 // export const fs = firebase;
-
 export const auth = getAuth();

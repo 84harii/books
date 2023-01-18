657aa6aec36869f4afca1294f91f152643521023
harii84 <99866727+harii84@users.noreply.github.com>
2023-01-18 21:31:14 +0530

    add shadow

--- src/component/hero.js
+++ src/component/hero.js
@@ -1,91 +1,47 @@
 import React from "react";
-import { Autoplay, Navigation, Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/autoplay";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  MdOutlineKeyboardArrowLeft,
-  MdOutlineNavigateNext,
-} from "react-icons/md";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+// import required modules
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper";
 
 const Hero = () => {
   return (
     <div className="hero section-padding-t section-bg pt-0">
-    
       {/* <img src="https://i.postimg.cc/TYX3pPQK/golden-leaf.png" className="leaf_one" alt="background_decorative_image"/> */}
       <div className="container">
         <div className="row testi-row">
           <div className="col-12 ps-0 pe-0">
-           <Swiper>
+            <Swiper
+              spaceBetween={30}
+              effect={"fade"}
+              navigation={false} 
+              className="mySwiper"
+              modules={[EffectFade,Autoplay, Pagination, Navigation]}
+              loop={true}
+              loopFillGroupWithBlank={true}  
+              autoplay={{
+                delay: 4500,
+                disableOnInteraction: false,
+              }}
+            >
               <SwiperSlide>
-                <div className="row align-items-center">
-                  <div className="col-lg-6 mb-4 mb-lg-0">
-                    {/* <div className="hero__content position-relative">
-                      <div className="badge-text mb-2 text-uppercase">
-                        LET'S MAKE THE BEST INVESTMENT
-                      </div>
-                      <h1 className="display-4 mb-4 text-capitalize">
-                        There Is No Friend As Loyal As A Book
-                      </h1>
-                      <p className="text-muted mb-5 fs-5">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Ad harum quibusdam, assumenda quia explicabo.
-                      </p>
-                      <Link to="/all-books" className="button button__primary">
-                        <span>Shop now</span>
-                      </Link>
-                    </div> */}
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="hero__images text-center">
-                      <img
-                        className="img-fluid"
-                        src="https://cdn.shopify.com/s/files/1/0681/8689/8738/files/91352_2_2000x.webp?v=1671902988"
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                </div>
+                <img  src="https://images.pexels.com/photos/15021623/pexels-photo-15021623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="slider_img"/>
               </SwiperSlide>
               <SwiperSlide>
-                <div className="row align-items-center">
-                  <div className="col-lg-6 mb-4 mb-lg-0">
-                    {/* <div className="hero__content position-relative">
-                      <div className="badge-text mb-2 text-uppercase">
-                        LET'S MAKE THE BEST INVESTMENT
-                      </div>
-                      <h1 className="display-4 mb-4 text-capitalize">
-                        There Is No Friend As Loyal As A Book
-                      </h1>
-                      <p className="text-muted mb-5 fs-5">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Ad harum quibusdam, assumenda quia explicabo.
-                      </p>
-                      <Link to="/all-books" className="button button__primary">
-                        <span>Shop now</span>
-                      </Link>
-                    </div> */}
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="hero__images text-center">
-                      <img
-                        className="img-fluid"
-                        src="https://cdn.shopify.com/s/files/1/0681/8689/8738/files/91352_2_2000x.webp?v=1671902988"
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                </div>
+                <img  src="https://images.pexels.com/photos/599619/pexels-photo-599619.jpeg?auto=compress&cs=tinysrgb&w=1600" className="slider_img"/>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img  src="https://images.pexels.com/photos/11403744/pexels-photo-11403744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="slider_img"/>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img  src="https://images.pexels.com/photos/3067112/pexels-photo-3067112.jpeg?auto=compress&cs=tinysrgb&w=1600" className="slider_img"/>
               </SwiperSlide>
             </Swiper>
-            <div className="prev">
-              <MdOutlineKeyboardArrowLeft />
-            </div>
-            <div className="next">
-              <MdOutlineNavigateNext />
-            </div>
           </div>
         </div>
       </div>

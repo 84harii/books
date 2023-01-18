f8b9467da0ca89bcbb5573da34516ad6f62411c2
harii84 <99866727+harii84@users.noreply.github.com>
2023-01-18 08:47:23 +0530

    Revert "remove mantine ui"
    This reverts commit 17ea53b6a6c9bf39644472db1f8357dfbe070cdb.

--- src/component/hero.js
+++ src/component/hero.js
@@ -16,7 +16,14 @@ const Hero = () => {
     
       {/* <img src="https://i.postimg.cc/TYX3pPQK/golden-leaf.png" className="leaf_one" alt="background_decorative_image"/> */}
       <div className="container">
-        <div className="row testi-row"> 
+        <div className="row testi-row">
+<<<<<<< HEAD
+          <div className="col-12 ps-0 pe-0">
+            <Carousel slideSize="70%" height={200} slideGap="md"
+                  align="center"  
+                  slidesToScroll={1}
+                  loop  withControls={false}
+=======
           <div className="col-12">
             <Swiper
               modules={[Navigation, Pagination, Autoplay]}
@@ -27,7 +34,8 @@ const Hero = () => {
                 prevEl: ".prev",
                 nextEl: ".next",
               }}
-              className="hero__swiper pb-5" 
+              className="hero__swiper pb-5"
+>>>>>>> parent of d654786 (add mantine ui)
             >
               <SwiperSlide>
                 <div className="row align-items-center">

eca41dc0fc9786b51d25c46aada757dd99219c4a
harii84 <99866727+harii84@users.noreply.github.com>
2023-01-27 08:41:38 +0530

    add image link in add book

--- src/component/open-modal.js
+++ src/component/open-modal.js
@@ -12,19 +12,19 @@ import { useAllContext } from "./context/context";
 import logo from "../component/global/RD-Luxurious-logo_1.svg";
 import { IoMdArrowBack } from "react-icons/io";
 import { CgShoppingBag } from "react-icons/cg";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/effect-fade";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-// import required modules
-import { Autoplay, EffectFade, Navigation, Pagination } from "swiper";
+import "swiper/css/effect-creative";
 
+// import required modules
+import { Autoplay, EffectCreative, Navigation, Pagination } from "swiper";
+import { HiOutlineEmojiHappy } from "react-icons/hi";
+ 
 const OpenModal = ({ book, handleRemove, handleChange, addToCart }) => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -36,12 +36,12 @@ const OpenModal = ({ book, handleRemove, handleChange, addToCart }) => {
   const { cart } = useAllContext();
   const showToastMessage = () => {
     toast.success(`${book.title} added to cart`, {
-        position: toast.POSITION.TOP_RIGHT
+      position: toast.POSITION.TOP_RIGHT,
     });
-};
+  };
 
-  return ( 
-    <> 
+  return (
+    <>
       <div className="icon" onClick={handleShow}>
         <MdZoomOutMap />
       </div>
@@ -54,11 +54,12 @@ const OpenModal = ({ book, handleRemove, handleChange, addToCart }) => {
         centered
       >
         <div className="modal__wrapper">
-          <div className="modal__wrapper--top">
+          <div className="modal__wrapper--top position-relative">
             <span className="close" onClick={handleClose}>
               <IoMdArrowBack />
             </span>
             <img src={logo} alt="RD" width={46} />
+            <img src="https://i.postimg.cc/PxC0xmYh/pattern-17.png" className="rotate_animation"/>
             <span style={{ color: "#00000000" }}>........</span>
           </div>
           <div className="row modal__wrapper__bottom">
@@ -67,7 +68,32 @@ const OpenModal = ({ book, handleRemove, handleChange, addToCart }) => {
                 className="img-fluid image_card"
                 style={{ backgroundImage: `url(${book.img})` }}
               ></div> */}
-                 <Swiper
+              <Swiper
+                grabCursor={true}
+                effect={"creative"}
+                creativeEffect={{
+                  prev: {
+                    shadow: true,
+                    translate: [0, 0, -800],
+                    rotate: [180, 0, 0],
+                  },
+                  next: {
+                    shadow: true,
+                    translate: [0, 0, -800],
+                    rotate: [-180, 0, 0],
+                  },
+                }}
+                modules={[EffectCreative,Autoplay, Pagination, Navigation]}
+                className="mySwiper4"
+                loop={true}
+                loopFillGroupWithBlank={true}
+                autoplay={{
+                  delay: 500,
+                  disableOnInteraction: true,
+                }}
+                
+              >
+                {/* <Swiper
               spaceBetween={30}
               effect={"fade"}
               navigation={false} 
@@ -79,23 +105,39 @@ const OpenModal = ({ book, handleRemove, handleChange, addToCart }) => {
                 delay: 1500,
                 disableOnInteraction: false,
               }}
-            >
-              <SwiperSlide>
-              <img className="img-fluid" src={book.img} alt={book.title} />
-              </SwiperSlide>
-              <SwiperSlide>
-              <img className="img-fluid" src={book.image1} alt={book.title} />
-              </SwiperSlide>
-              <SwiperSlide>
-              <img className="img-fluid" src={book.image2} alt={book.title} />
-              </SwiperSlide>
-              <SwiperSlide>
-              <img className="img-fluid" src={book.image3} alt={book.title} />
-              </SwiperSlide>
-              <SwiperSlide>
-              <img className="img-fluid" src={book.image4} alt={book.title} />
-              </SwiperSlide>
-            </Swiper> 
+            > */}
+                <SwiperSlide>
+                  <img className="img-fluid img"  src={book.img} alt={book.title} />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    className="img-fluid img" 
+                    src={book.image1}
+                    alt={book.title}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    className="img-fluid img" 
+                    src={book.image2}
+                    alt={book.title}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    className="img-fluid img" 
+                    src={book.image3}
+                    alt={book.title}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    className="img-fluid img" 
+                    src={book.image4}
+                    alt={book.title}
+                  />
+                </SwiperSlide>
+              </Swiper>
             </div>
             <div className="col-lg-6">
               <h3>{book.title}</h3>
@@ -177,11 +219,14 @@ const OpenModal = ({ book, handleRemove, handleChange, addToCart }) => {
                     <span> Add to cart</span>
                   </span>
                 </button>
-                
               )}
-            </div> 
-            
-            <ToastContainer icon={false} position="top-center"autoClose={2500}/>
+            </div>
+
+            <ToastContainer
+              icon={false}
+              position="top-center"
+              autoClose={2500}
+            />
           </div>
         </div>
       </Modal>
